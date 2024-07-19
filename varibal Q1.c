542f8d84ace6ad69ea841d860e8b6dd02edace69
anjalgajera8436 <8436anjalgajera@gmail.com>
2024-07-19 12:28:18 +0530

    Add files via upload

---
+++ varibal Q1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+ main()
+ {
+     int x;
+	 int y;
+	 x=12;
+	 y=6;
+	 
+	 printf ("%d + %d = %d\n",x,y,x+y);	
+	 printf ("%d - %d = %d\n",x,y,x-y);
+	 printf ("%d * %d = %d\n",x,y,x*y);
+	 printf ("%d / %d = %d\n",x,y,x/y);
+	 printf ("x % y = %d\n",x%y);
+    
+	  	
+}
---
+++ varibal Q2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+ main()
+{
+	int x;
+	int y;
+	x=5;
+	y=5;
+	printf("---------\n");
+	printf("|        |\n");
+	printf("|%d * %d = %d|\n",x,y,x*y);
+	printf("|        |\n");
+	printf("----------\n");
+}
