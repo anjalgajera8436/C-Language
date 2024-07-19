faf90d36c619a6313bae2a6d7132680002df10a1
anjalgajera8436 <8436anjalgajera@gmail.com>
2024-07-19 12:27:03 +0530

    Add files via upload

---
+++ printf  q1.c.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>  
+
+void main()
+{
+	printf("Name:- Gajera Anjal Ramnikbhai\n");
+	printf("Age:-17\n");
+	printf("School Name:-Leo School Of Sci And Com\n");
+	
+}
